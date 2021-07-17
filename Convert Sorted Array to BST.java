8d7aff9287bcc282e9e849881b47295de0795866
Mayur K J Parekh <MayurKJParekh@users.noreply.github.com>
2021-07-17 21:53:39 +0530

    Update and rename leetcode 108 Convert Sorted Array to Binary Search Tree.java to Convert Sorted Array to BST.java

--- Convert Sorted Array to BST.java
+++ Convert Sorted Array to BST.java
@@ -22,3 +22,13 @@ public class Solution {
     	return node;
     }
 }
+
+/*
+Input: nums = [-10,-3,0,5,9]
+Output: [0,-3,9,-10,null,5]
+Explanation: [0,-10,5,null,-3,null,9] is also accepted:
+
+Input: nums = [1,3]
+Output: [3,1]
+Explanation: [1,3] and [3,1] are both a height-balanced BSTs.
+*/
