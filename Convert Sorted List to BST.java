33d72c2c16d1fd06077d576c3223a5cb5947a348
Mayur K J Parekh <MayurKJParekh@users.noreply.github.com>
2021-07-17 21:56:29 +0530

    Update and rename leetcode 109 Convert Sorted List to Binary Search Tree.java to Convert Sorted List to BST.java

--- Convert Sorted List to BST.java
+++ Convert Sorted List to BST.java
@@ -24,3 +24,18 @@ public class Solution {
     	return thread;
     }
 }
+
+/*
+Input: head = [-10,-3,0,5,9]
+Output: [0,-3,9,-10,null,5]
+Explanation: One possible answer is [0,-3,9,-10,null,5], which represents the shown height balanced BST.
+
+Input: head = []
+Output: []
+
+Input: head = [0]
+Output: [0]
+
+Input: head = [1,3]
+Output: [3,1]
+*/
