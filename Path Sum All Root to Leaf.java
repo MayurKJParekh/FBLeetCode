f87ff920b41d14caee6c6390890f53c23bc48700
Mayur K J Parekh <MayurKJParekh@users.noreply.github.com>
2021-07-17 23:16:48 +0530

    Update and rename leetcode 113 Path Sum II.java to Path Sum All Root to Leaf.java

--- Path Sum All Root to Leaf.java
+++ Path Sum All Root to Leaf.java
@@ -26,3 +26,19 @@ public class Solution {
         cur.remove(cur.size() - 1);
     }
 }
+
+/*
+Given the root of a binary tree and an integer targetSum, return all root-to-leaf paths where each path's sum equals targetSum.
+Example 1:
+
+Input: root = [5,4,8,11,null,13,4,7,2,null,null,5,1], targetSum = 22
+Output: [[5,4,11,2],[5,8,4,5]]
+Example 2:
+
+Input: root = [1,2,3], targetSum = 5
+Output: []
+Example 3:
+
+Input: root = [1,2], targetSum = 0
+Output: []
+*/
