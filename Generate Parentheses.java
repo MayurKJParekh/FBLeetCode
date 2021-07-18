a0b7ba4943008adbe140c9fa882f9e7a6259ba35
Mayur K J Parekh <MayurKJParekh@users.noreply.github.com>
2021-07-18 17:53:05 +0530

    Update and rename leetcode 22 Generate Parentheses.java to Generate Parentheses.java

--- Generate Parentheses.java
+++ Generate Parentheses.java
@@ -27,3 +27,14 @@ public class Solution {
         }
     }
 }
+
+/*
+Example 1:
+
+Input: n = 3
+Output: ["((()))","(()())","(())()","()(())","()()()"]
+Example 2:
+
+Input: n = 1
+Output: ["()"]
+*/
