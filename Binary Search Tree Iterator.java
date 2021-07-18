aa3e5f632ff61216053ce0d61704de1a9274798b
Mayur K J Parekh <MayurKJParekh@users.noreply.github.com>
2021-07-18 16:08:55 +0530

    Update and rename leetcode 173 Binary Search Tree Iterator.java to Binary Search Tree Iterator.java

---
+++ Binary Search Tree Iterator.java
@@ -0,0 +1,33 @@
+//leetcode 173 Binary Search Tree Iterator
+
+/*
+Time: Constructor: O(h), next(): )O(h), hasNext(): O(1)
+Space: O(h)
+*/
+
+class BSTIterator {
+  public BSTIterator(TreeNode root) {
+    pushLeftsUntilNull(root);
+  }
+
+  /** @return the next smallest number */
+  public int next() {
+    TreeNode root = stack.pop();
+    pushLeftsUntilNull(root.right);
+    return root.val;
+  }
+
+  /** @return whether we have a next smallest number */
+  public boolean hasNext() {
+    return !stack.isEmpty();
+  }
+
+  private Stack<TreeNode> stack = new Stack<>();
+
+  private void pushLeftsUntilNull(TreeNode root) {
+    while (root != null) {
+      stack.push(root);
+      root = root.left;
+    }
+  }
+}
