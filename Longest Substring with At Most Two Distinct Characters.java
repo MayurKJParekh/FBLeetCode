859d445300269f0d0ad6588ccae5f5140d9b0550
Mayur K J Parekh <MayurKJParekh@users.noreply.github.com>
2021-07-18 15:20:59 +0530

    Update and rename leetcode 159 Longest Substring with At Most Two Distinct Characters.java to Longest Substring with At Most Two Distinct Characters.java

---
+++ Longest Substring with At Most Two Distinct Characters.java
@@ -0,0 +1,25 @@
+//leetcode 159 Longest Substring with At Most Two Distinct Characters
+
+/*
+time: O(n)
+space: O(128)
+*/
+
+class Solution {
+  public int lengthOfLongestSubstringTwoDistinct(String s) {
+    int ans = 0;
+    int distinct = 0;
+    int[] count = new int[128];
+
+    for (int l = 0, r = 0; r < s.length(); ++r) {
+      if (++count[s.charAt(r)] == 1)
+        ++distinct;
+      while (distinct == 3)
+        if (--count[s.charAt(l++)] == 0)
+          --distinct;
+      ans = Math.max(ans, r - l + 1);
+    }
+
+    return ans;
+  }
+}
