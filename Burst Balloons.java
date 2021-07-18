a738e10850494c38d46edaeecd1796734ce25d08
Mayur K J Parekh <MayurKJParekh@users.noreply.github.com>
2021-07-18 22:07:30 +0530

    Update and rename leetcode 312 Burst Balloons.java to Burst Balloons.java

--- Burst Balloons.java
+++ Burst Balloons.java
@@ -34,3 +34,17 @@ class Solution {
         return max;
     }
 }
+
+/*
+Example 1:
+
+Input: nums = [3,1,5,8]
+Output: 167
+Explanation:
+nums = [3,1,5,8] --> [3,5,8] --> [3,8] --> [8] --> []
+coins =  3*1*5    +   3*5*8   +  1*3*8  + 1*8*1 = 167
+Example 2:
+
+Input: nums = [1,5]
+Output: 10
+*/
