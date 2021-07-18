cbd8a7252f6bb314da41b748125c10144d926f34
Mayur K J Parekh <MayurKJParekh@users.noreply.github.com>
2021-07-18 18:32:10 +0530

    Update and rename leetcode 230 Kth Smallest Element in a BST.java to Kth Smallest Element in a BST.java

--- Kth Smallest Element in a BST.java
+++ Kth Smallest Element in a BST.java
@@ -45,3 +45,28 @@ class Solution {
         return root.val;    
     }
 }
+
+Stack
+O(n) 
+    O(k)
+class Solution {
+  public int kthSmallest(TreeNode root, int k) {
+    Stack<TreeNode> stack = new Stack<>();
+
+    while (root != null) {
+      stack.push(root);
+      root = root.left;
+    }
+
+    for (int i = 0; i < k - 1; ++i) {
+      root = stack.pop();
+      root = root.right;
+      while (root != null) {
+        stack.push(root);
+        root = root.left;
+      }
+    }
+
+    return stack.peek().val;
+  }
+}
