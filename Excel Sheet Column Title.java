a69a74423a6406e6eb6925d1f299851272985e1d
Mayur K J Parekh <MayurKJParekh@users.noreply.github.com>
2021-07-18 16:02:35 +0530

    Update and rename leetcode 168 Excel Sheet Column Title.java to Excel Sheet Column Title.java

--- Excel Sheet Column Title.java
+++ Excel Sheet Column Title.java
@@ -16,3 +16,34 @@ public class Solution {
         return res.reverse().toString();
     }
 }
+
+/*
+For example:
+
+A -> 1
+B -> 2
+C -> 3
+...
+Z -> 26
+AA -> 27
+AB -> 28 
+...
+ 
+
+Example 1:
+
+Input: columnNumber = 1
+Output: "A"
+Example 2:
+
+Input: columnNumber = 28
+Output: "AB"
+Example 3:
+
+Input: columnNumber = 701
+Output: "ZY"
+Example 4:
+
+Input: columnNumber = 2147483647
+Output: "FXSHRXW"
+*/
